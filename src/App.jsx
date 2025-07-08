0dcaf146d59ae4f88ef9a9d400cb4d41a8527e9e
Shaaravkamdi <shaaravkamdi0408>
2025-07-08 11:27:11 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 
 import Login from './components/auth/Login';
